acca5ed408fc1068aeabfb4069e4170c97dc5efd
armandfardeau <fardeauarmand@gmail.com>
2023-03-07 12:28:01 +0100

    Improve coverage

--- spec/lib/bonobot/overloads_registry_spec.rb
+++ spec/lib/bonobot/overloads_registry_spec.rb
@@ -30,4 +30,30 @@ describe Bonobot::OverloadsRegistry do
       expect(registry.output).to be_an(Array)
     end
   end
+
+  describe ".find_by" do
+    let(:path) { "spec/test_files/example_file.rb" }
+
+    it "returns an array" do
+      expect(registry.find_by(path: path)).to be_an(Array)
+      expect(registry.find_by(path: path).first).to be_a(Bonobot::Overload)
+      expect(registry.find_by(path: path).count).to eq(1)
+    end
+
+    context "when no file is found" do
+      let(:path) { "spec/test_files/non_existent_file.rb" }
+
+      it "returns an empty array" do
+        expect(registry.find_by(path: path)).to be_an(Array)
+        expect(registry.find_by(path: path).count).to eq(0)
+      end
+    end
+
+    context "when key is not found" do
+      it "returns an empty array" do
+        expect(registry.find_by(non_existent_key: path)).to be_an(Array)
+        expect(registry.find_by(non_existent_key: path).count).to eq(0)
+      end
+    end
+  end
 end
